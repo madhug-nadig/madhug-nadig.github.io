a1661f5c142fae22cfe416f357bf8388eb2a8a16
Madhu G Nadig <madhug.nadig@gmail.com>
2017-01-05 06:28:53 +0530

    Stuff

--- js/index.js
+++ js/index.js
@@ -5,13 +5,6 @@ new WOW().init();
 // $( window ).scroll(function() {
 //   $('.nav').addClass('navScroll');
 // });
-
-$('#blog-trig').click(function() {
-  $('.blog').addClass('blog-active');
-  $('.overlay').fadeIn();
-  $('body').addClass('lock');
-});
-
 $('.overlay').click(function() {
   $('.blog').removeClass('blog-active');
   $('.overlay').fadeOut();
