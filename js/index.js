334c44b8b6b379a1f8baa913b841e90a60225e8a
Madhu G Nadig <madhug.nadig@gmail.com>
2017-01-07 11:59:01 +0530

    Some changes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,1 @@
-$('html, body').localScroll({duration:800});
-
 new WOW().init();
-
-// $( window ).scroll(function() {
-//   $('.nav').addClass('navScroll');
-// });
-$('.overlay').click(function() {
-  $('.blog').removeClass('blog-active');
-  $('.overlay').fadeOut();
-  $('body').removeClass('lock');
-});
